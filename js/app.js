f890ee820906f308ca7a7fb383d9262a89754adb
Matthew Austin <maustin803@gmail.com>
2022-10-29 13:22:26 -0700

    Add console log test

--- js/app.js
+++ js/app.js
@@ -11,9 +11,12 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
   const sum = a + b;
   return [sum, `The sum of ${a} and ${b} is ${sum}.`];
+}
+
+console.log(sum(4,7));
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
